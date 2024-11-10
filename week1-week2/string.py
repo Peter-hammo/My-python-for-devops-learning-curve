84ddc8ae8d52e9569f12d2b80d0b19c5a107926d
Peter-hammo <peterhammond3965@gmail.com>
2024-11-10 06:14:42 +0300

    Update string.py

--- week1-week2/string.py
+++ week1-week2/string.py
@@ -1,42 +1,42 @@
-examples are follows:
+#examples are follows:
 
-Concatenation
+#Concatenation
 str1 = "Hello"
 str2 = "World"
 result = str1 + " " + str2
 print(result)
 
 
-string length
+#string length
 text = "Python is awesome"
 length = len(text)
 print("Length of the string:", length)
 
- uppercase and lowercase
+#uppercase and lowercase
 text = "Python is awesome"
 uppercase = text.upper()
 lowercase = text.lower()
 print("Uppercase:", uppercase)
 print("Lowercase:", lowercase)
 
-replacing
+#replacing
 text = "Python is awesome"
 new_text = text.replace("awesome", "great")
 print("Modified text:", new_text)
 
 
-spltting
+#spltting
 text = "Python is awesome"
 words = text.split()
 print("Words:", words)
 
-strip text
+#strip text
 text = "   Some spaces around   "
 stripped_text = text.strip()
 print("Stripped text:", stripped_text)
 
 
-substring
+#substring
 text = "Python is awesome"
 substring = "is"
 if substring in text:
